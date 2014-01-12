205215f486924fcbb3a2e6a82bec63371bf25c8a
Ben Cox <Ben@benjojo.co.uk>
2014-01-12 01:58:00 +0000

    Add que ops, Also retabbed everything back to sanity

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 # include <unistd.h>
 # include <sys/socket.h>
 # include <sys/types.h>
+# include <sys/queue.h>
 # include <string.h>
 # include <netinet/in.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <arpa/inet.h>
 # include <netdb.h>
+# include <stdbool.h>
+# include <sys/queue.h>
 
 
 # include <netinet/in.h>
@@ -20,25 +23,25 @@
 
 static int in_cksum(unsigned short *buf, int sz)
 {
-  int nleft = sz;
-  int sum = 0;
-  unsigned short *w = buf;
-  unsigned short ans = 0;
- 
-  while (nleft > 1) {
-    sum += *w++;
-    nleft -= 2;
-  }
- 
-  if (nleft == 1) {
-    *(unsigned char *) (&ans) = *(unsigned char *) w;
-    sum += ans;
-  }
- 
-  sum = (sum >> 16) + (sum & 0xFFFF);
-  sum += (sum >> 16);
-  ans = ~sum;
-  return (ans);
+    int nleft = sz;
+    int sum = 0;
+    unsigned short *w = buf;
+    unsigned short ans = 0;
+
+    while (nleft > 1) {
+        sum += *w++;
+        nleft -= 2;
+    }
+
+    if (nleft == 1) {
+        *(unsigned char *) (&ans) = *(unsigned char *) w;
+        sum += ans;
+    }
+
+    sum = (sum >> 16) + (sum & 0xFFFF);
+    sum += (sum >> 16);
+    ans = ~sum;
+    return (ans);
 }
 
 static char *hostname = NULL;
@@ -82,65 +85,108 @@ main(){
                 IPSrc[i-12] = buf[i];
             }
             for (i = 0; i < 4; i++) {
-                // for debugging reasons. Print the IP out.
-                printf("%02X%s", (uint8_t)IPSrc[i], (i + 1)%16 ? " " : "\n");
-            }
-            int pingsock, c;
-            if ((pingsock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0) {
-                perror("ping: creating a raw socket to reply with! :(");
-                // exit(1);
-            } else {
-                struct sockaddr_in pingaddr;
-                struct icmp *pkt;
-                struct hostent *h;
-                char packet[n - 20];
-                // Going to read out the old IPv4 packet and change the reply code and then
-                // blank out the checksum.
-                for(i = ip_hdr->ihl*4; i < n; i++) {
-                    packet[i-ip_hdr->ihl*4] = buf[i];
-                }
-                packet[0] = 0;
-                packet[2] = 0;
-                packet[3] = 0;
+            // for debugging reasons. Print the IP out.
+            printf("%02X%s", (uint8_t)IPSrc[i], (i + 1)%16 ? " " : "\n");
+        }
+        int pingsock, c;
+        if ((pingsock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) < 0) {
+            perror("ping: creating a raw socket to reply with! :(");
+            // exit(1);
+        } else {
+        struct sockaddr_in pingaddr;
+        struct icmp *pkt;
+        struct hostent *h;
+        char packet[n - 20];
+        // Going to read out the old IPv4 packet and change the reply code and then
+        // blank out the checksum.
+        for(i = ip_hdr->ihl*4; i < n; i++) {
+            packet[i-ip_hdr->ihl*4] = buf[i];
+        }
+        packet[0] = 0;
+        packet[2] = 0;
+        packet[3] = 0;
 
-                pingaddr.sin_family = AF_INET;
-                char full[50];
-                sprintf(full, "%d.%d.%d.%d", (int)IPSrc[0], (int)IPSrc[1], (int)IPSrc[2], (int)IPSrc[3]);
-                fprintf(stderr, "ping: Got a ping from %s\n", full);
-                if (!(h = gethostbyname(full))) {
-                    fprintf(stderr, "ping: unknown host %s\n", full);
-                    exit(1);
-                }
-                memcpy(&pingaddr.sin_addr, h->h_addr, sizeof(pingaddr.sin_addr));
-                hostname = h->h_name;
+        pingaddr.sin_family = AF_INET;
+        char full[50];
+        sprintf(full, "%d.%d.%d.%d", (int)IPSrc[0], (int)IPSrc[1], (int)IPSrc[2], (int)IPSrc[3]);
+        fprintf(stderr, "ping: Got a ping from %s\n", full);
+        if (!(h = gethostbyname(full))) {
+            fprintf(stderr, "ping: unknown host %s\n", full);
+            exit(1);
+        }
+        memcpy(&pingaddr.sin_addr, h->h_addr, sizeof(pingaddr.sin_addr));
+        hostname = h->h_name;
 
-                pkt = (struct icmp *) packet;
-                // memset(pkt, 0, sizeof(packet));
-                // pkt->icmp_type = ICMP_ECHOREPLY;
-                pkt->icmp_cksum = in_cksum((unsigned short *) pkt, sizeof(packet));
+        pkt = (struct icmp *) packet;
+        // memset(pkt, 0, sizeof(packet));
+        // pkt->icmp_type = ICMP_ECHOREPLY;
+        pkt->icmp_cksum = in_cksum((unsigned short *) pkt, sizeof(packet));
 
-                printf("wat %d",sizeof(packet));
-                // pkt->icmp_sequence = 
-                c = sendto(pingsock, packet, sizeof(packet), 0,
-                    (struct sockaddr *) &pingaddr, sizeof(struct sockaddr_in));
-                if (c < 0 || c != sizeof(packet)) {
-                    if (c < 0)
-                        perror("ping: sendto");
-                    fprintf(stderr, "ping: write incomplete\n");
-                    // exit(1);
-                }
+        printf("wat %d",sizeof(packet));
+        // pkt->icmp_sequence = 
+        c = sendto(pingsock, packet, sizeof(packet), 0,
+            (struct sockaddr *) &pingaddr, sizeof(struct sockaddr_in));
+        if (c < 0 || c != sizeof(packet)) {
+            if (c < 0)
+                perror("ping: sendto");
+            fprintf(stderr, "ping: write incomplete\n");
+            // exit(1);
+        }
 
-                close(pingsock);
-            }
+        close(pingsock);
+        }
 
-            /*
-                Okay so, the idea goes is that you have a stack (that has a ~400 entry limit)
-                with the source IP and the time.
+        /*
+        Okay so, the idea goes is that you have a stack (that has a ~400 entry limit)
+        with the source IP and the time.
 
-                When a ping arrives it will scan thought the stack and check if it has 2 
-                entries of that source IP, if it does it will calcualte the avg diff between requests
-                and will schedule a reply -5ms before it expects the next one to arrive.
-            */
+        When a ping arrives it will scan thought the stack and check if it has 2 
+        entries of that source IP, if it does it will calcualte the avg diff between requests
+        and will schedule a reply -5ms before it expects the next one to arrive.
+        */
         }
     }
+}
+
+
+struct entry {
+    int value;
+    TAILQ_ENTRY(entry) entries;
+};
+
+typedef struct entry entry_t;
+
+TAILQ_HEAD(FIFOList_s, entry);
+
+typedef struct FIFOList_s FIFOList;
+
+
+bool m_enqueue(int v, FIFOList *l)
+{
+    entry_t *val;
+    val = malloc(sizeof(entry_t));
+    if ( val != NULL ) {
+        val->value = v;
+        TAILQ_INSERT_TAIL(l, val, entries);
+        return true;
+    }
+    return false;
+}
+
+bool m_dequeue(int *v, FIFOList *l)
+{
+    entry_t *e = l->tqh_first;
+    if ( e != NULL ) {
+        *v = e->value;
+        TAILQ_REMOVE(l, e, entries);
+        free(e);
+        return true;
+    }
+    return false;
+}
+
+bool isQueueEmpty(FIFOList *l)
+{
+    if ( l->tqh_first == NULL ) return true;
+    return false;
 }
